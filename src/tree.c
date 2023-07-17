65dc5a43f1ceb9c70f97461639f2ed2dbfbc4016
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-07-17 14:45:55 +0100

    Introduce left-child right-sibling (lcrs) tree

--- src/tree.c
+++ src/tree.c
@@ -1,1 +1,9 @@
+#include <stdlib.h>
+
 #include "tree.h"
+void lcrs_node_init(struct lcrs_node *node) {
+    node->data = 0;
+    node->child = NULL;
+    node->sibling = NULL;
+}
+
--- src/tree.h
+++ src/tree.h
@@ -1,6 +1,12 @@
 #ifndef TREE_H
 #define TREE_H
 
+struct lcrs_node {
+    int data;
+    struct lcrs_node *child;
+    struct lcrs_node *sibling;
+};
 
+void lcrs_node_init(struct lcrs_node *node);
 
 #endif
